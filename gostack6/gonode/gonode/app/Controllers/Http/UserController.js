194bfcdb5afe551355735f835af164e5a782554e
Joberth Rogers <joberth.rogers18@gmail.com>
2020-01-28 01:28:11 -0300

    Created controllers and store users on database

---
+++ gostack6/gonode/gonode/app/Controllers/Http/UserController.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+  async store ({ request }) {
+    // Pegando do body
+    const data = request.only([
+      'username',
+      'email',
+      'password'
+    ])
+
+    const user = await User.create(data)
+
+    return user
+  }
+}
+
+module.exports = UserController
