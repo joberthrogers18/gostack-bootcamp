daa808251c57064aa2c6567da431e656578e9683
Joberth Rogers <joberth.rogers18@gmail.com>
2020-02-02 23:47:30 -0300

    Added Route to get file from tmp upload directory
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- gostack6/gonode/gonode/app/Models/File.js
+++ gostack6/gonode/gonode/app/Models/File.js
@@ -2,8 +2,17 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Env = use('Env')
 
 class File extends Model {
+  // atributo virtual, não existe na tabela do banco
+  static get computed () {
+    return ['url']
+  }
+
+  getUrl ({ id }) {
+    return `${Env.get('APP_URL')}/files/${id}`
+  }
 }
 
 module.exports = File
